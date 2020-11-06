e7a0324c04a62855612230684ebc783a61c9ff57
Perry Sittser <sittserp@gmail.com>
2020-11-05 19:08:00 -0800

    update working

--- src/Detail.js
+++ src/Detail.js
@@ -14,29 +14,36 @@ export default class Create extends Component {
     state = {
         types: []
     }
-
     componentDidMount = async () => {
         const types = await fetchTypes();
+        const tree = await fetchTree(this.props.match.params.id);
 
+        const typeAsAString = tree.type;
 
-        this.setState({ types: response.body })
+        const matchingType = types.find((type) => {
+            return type.type === typeAsAString
+        });
+
+        this.setState({
+            types: types,
+            typeId: matchingType.id,
+            hardnessFactor: tree.hardness_factor,
+            hardwood: tree.hardwood,
+            name: tree.name
+        });
     }
 
     handleSubmit = async (e) => {
         e.preventDefault();
 
-        const newTree = {
-            name: this.state.name,
-            hardness_factor: this.state.hardnessFactor,
-            hardwood: this.state.hardwood,
-            type_id: this.state.typeId,
-            owner_id: userFromLocalStorage.userId
-            // work!
-        };
-
-        await fetch
-            .post('https://frozen-escarpment-09808.herokuapp.com/trees')
-            .send(newTree);
+        await updateTree(this.props.match.params.id,
+            {
+                name: this.state.name,
+                hardness_factor: this.state.hardnessFactor,
+                hardwood: this.state.hardwood,
+                type_id: this.state.typeId,
+                owner_id: userFromLocalStorage.userId
+            });
 
         this.props.history.push('/');
     }
@@ -48,27 +55,49 @@ export default class Create extends Component {
     render() {
         return (
             <div>
-                Add a new tree
+                Update this tree
                 <form onSubmit={this.handleSubmit}>
                     <label>
                         name
-        <input onChange={e => this.setState({ name: e.target.value })} type="string" />
+                            <input
+                            value={this.state.name}
+                            onChange={e => this.setState({ name: e.target.value })}
+                            type="string" />
                     </label>
                     <label>
                         hardness factor
-        <input onChange={e => this.setState({ hardnessFactor: e.target.value })} type="number" />
+                            <input
+                            value={this.state.hardnessFactor}
+                            onChange={e => this.setState({ hardnessFactor: e.target.value })}
+                            type="number" />
                     </label>
                     <label>
                         hardwood? true:
-        <input name="hardwood" type="radio" value="true" onChange={e => this.setState({ hardwood: e.target.value })} />
-                        false:<input name="hardwood" type="radio" value="false" onChange={e => this.setState({ hardwood: e.target.value })} />
+                            <input
+                            name="hardwood"
+                            type="radio"
+                            value="true"
+                            checked={this.state.hardwood === true}
+                            onChange={e => this.setState({ hardwood: e.target.value })} />
+                        false:
+                            <input
+                            name="hardwood"
+                            type="radio"
+                            value="false"
+                            checked={this.state.hardwood === false}
+                            onChange={e => this.setState({ hardwood: e.target.value })} />
                     </label>
                     <label>
                         type
         <select onChange={this.handleChange}>
                             <option></option>
                             {
-                                this.state.types.map(type => <option key={type.id} value={type.id}>{type.type}</option>)
+                                this.state.types.map(type => <option
+                                    selected={this.state.typeId === type.id}
+                                    key={type.id}
+                                    value={type.id}>
+                                    {type.type}
+                                </option>)
                             }
                         </select>
                     </label>
